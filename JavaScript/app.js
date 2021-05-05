52dc68946eb871de0a9c027835cf99ec7d632f63
BasharAQ1 <>
2021-05-05 12:08:12 +0000

    js folder

---
+++ JavaScript/app.js
@@ -0,0 +1,9 @@
+console.log("This is an Easter Egg")
+
+alert('Hello\nHow are you?')
+
+var nationality = prompt('What is your nationality?');
+
+if (nationality === "Jordanian") { document.write('<img src="https://ar.vogue.me/image_provider/?w=750&h=&zc=1&q=80&cc=ffffff&src=https://en.vogue.me/wp-content/uploads/2019/05/9.jpg"width="100%">'); }
+
+else if (nationality !== "Jordanian") { document.write("Welcome two our World Wide Website"); }
