6dfe7867cf07b37aa665c05f42be4f7113e92a57
BasharAQ1 <>
2021-05-09 12:04:54 +0000

    added a function and var

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -12,27 +12,29 @@ else if (nationality !== "Jordanian") { document.write("Welcome two our World Wi
 
 /* Beverages */
 
-var beverages = prompt("How do you like you beverages cold or hot?");
 
-console.log(beverages)
+var beverages = prompt("How do you like you beverages cold or hot?");
 
 var beverages = beverages.toLowerCase();
 
-console.log(beverages)
-
-
 while (beverages !== "cold" && beverages !== "hot") { beverages = prompt("Make sure to answer with eather Cold or Hot"); }
 
-while (beverages !== "cold" && beverages !== "hot") { drink = prompt("Make sure to answer with eather Cold or Hot"); }
+// while (beverages !== "cold" && beverages !== "hot") { drink = prompt("Make sure to answer with eather Cold or Hot"); }
 
 /* Service Rating */
+
+var serviceRating=function(){
 
 var food = prompt("How would you rate the value of our food? Rate from 1 (Poor Value) to 5 (Great Value)")
 
 if (food > 5) {
   food = 5
 }
+var starResults="";
 
 for (var i = 0; i < food; i++) {
-  document.write('<img src="https://image.shutterstock.com/image-vector/gold-star-elegant-260nw-238637683.jpg" width="50px">');
+  starResults= starResults + ('<img src="https://image.shutterstock.com/image-vector/gold-star-elegant-260nw-238637683.jpg" width="50px">');
 }
+return starResults
+}
+document.write (serviceRating())
